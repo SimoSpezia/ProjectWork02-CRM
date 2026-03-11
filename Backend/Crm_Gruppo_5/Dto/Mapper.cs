7f36d9a22e9b4d43f117cb3e81640d97c7fc23ed
SpezialeSimone <SpezialeSimone@CLOUDJ-SPEZIALE>
2026-03-11 14:42:01 +0100

    Aggiunta Mapper

---
+++ Backend/Crm_Gruppo_5/Dto/Mapper.cs
@@ -0,0 +1,24 @@
+﻿using CrmGruppo5.Data;
+using System.ComponentModel.Design;
+
+namespace Crm_Gruppo_5.Dto
+{
+    public class Mapper
+    {
+
+        public CompanyDto MapBaseEntitytoDto(Company entity)
+        {
+            CompanyDto dto = new CompanyDto
+            {
+                CompanyId= entity.CompanyId,
+                Denomination = entity.Denomination,
+                AddressId = entity.AddressId,
+                Website = entity.Website,
+                VatNumber = entity.VatNumber,
+                Size = entity.Size,
+                Note = entity.Note,
+            };
+            return dto;
+        }
+    }
+}
