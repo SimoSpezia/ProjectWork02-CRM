f1f5a387f5927f06596f239b9d0dd934f67d4960
AntonucciMichele <m.antonucci04@gmail.com>
2026-03-13 11:11:51 +0100

    Gestione cors

--- Backend/Crm_Gruppo_5/Program.cs
+++ Backend/Crm_Gruppo_5/Program.cs
@@ -8,28 +8,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options => {
     options.AddPolicy("FrontendPolicy", policy =>
     {
-        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500").AllowAnyHeader().AllowAnyMethod();
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
     });
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddSqlServer<ContactDbContext>(
     builder.Configuration.GetConnectionString("Default")
 );
 builder.Services.AddSingleton<Mapper>();
+
 var app = builder.Build();
 
 app.MapOpenApi();
-
 app.MapScalarApiReference();
 
+app.UseCors("FrontendPolicy");      
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
