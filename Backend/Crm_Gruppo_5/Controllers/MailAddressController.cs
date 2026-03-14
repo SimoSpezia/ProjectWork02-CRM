b13ba5c685c7a2f7c32965501d9e9ede3fddd0af
SpezialeSimone <SpezialeSimone@CLOUDJ-SPEZIALE>
2026-03-14 17:18:36 +0100

    Aggiunta CRUD Mailaddress

---
+++ Backend/Crm_Gruppo_5/Controllers/MailAddressController.cs
@@ -0,0 +1,83 @@
+﻿using Crm_Gruppo_5.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crm_Gruppo_5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailAddressController(Data.ContactDbContext ctx, ILogger<MailAddressController> logger, Mapper mapper) : ControllerBase
+    {
+
+        private readonly Data.ContactDbContext _ctx = ctx;
+        private readonly ILogger<MailAddressController> _logger = logger;
+        private readonly Mapper _mapper = mapper;
+
+        [HttpGet]
+        [Route("all")]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var result = _ctx.MailAddresses.ToList().ConvertAll(_mapper.MapBaseEntitytoDto);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            var mail = _ctx.MailAddresses.SingleOrDefault(m => m.MailAddressId == id);
+
+            if (mail == null)
+            {
+                return BadRequest($"MailAddress with id {id} not found");
+
+            }
+            return Ok(_mapper.MapBaseEntitytoDto(mail));
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] MailAddressDto Dto)
+        {
+            var mail = _ctx.MailAddresses.SingleOrDefault(m => m.MailAddressId == id);
+
+            if (mail == null)
+            {
+                return NotFound();
+            }
+            if (!string.IsNullOrEmpty(Dto.Mail))
+                mail.Mail = Dto.Mail;
+            _ctx.SaveChanges();
+
+            var result = _mapper.MapBaseEntitytoDto(mail);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var mail = _ctx.MailAddresses.SingleOrDefault(m => m.MailAddressId == id);
+
+            if (mail == null)
+            {
+                return NotFound();
+            }
+
+            _ctx.MailAddresses.Remove(mail);
+
+            if (_ctx.SaveChanges() > 0)
+                return NoContent();
+            else
+                return UnprocessableEntity("Impossibile eliminare la mail.");
+        }
+    }
+}
