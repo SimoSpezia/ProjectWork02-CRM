2b8279c97dd667aa064394c4d6244d22e34fea60
SpezialeSimone <SpezialeSimone@CLOUDJ-SPEZIALE>
2026-03-16 15:43:28 +0100

    risoluzione conflitti

--- Backend/Crm_Gruppo_5/Dto/Mapper.cs
+++ Backend/Crm_Gruppo_5/Dto/Mapper.cs
@@ -301,14 +301,5 @@ namespace Crm_Gruppo_5.Dto
             };
             return entity;
         }
-
-        public ContactType MapDtoToEntity(ContactTypeDto dto)
-        {
-            ContactType entity = new ContactType
-            {
-                Description = dto.Description
-            };
-            return entity;
-        }
     }
 }
